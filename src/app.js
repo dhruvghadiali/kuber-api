f7262fc89c3a9551562f1a7b9a9ab20434da25f4
dhruv ghadiali <dhruvghadiali@hotmail.com>
2024-06-22 16:03:11 +0530

    Updated env files

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 require("colors");
 const path = require("path");
-const dotenv = require("dotenv");
 const express = require("express");
 const mongoose = require("mongoose");
 var cors = require('cors');
@@ -9,8 +8,6 @@ const errorHandler = require("./middleware/error");
 const commonRouter = require("./router/commonRouter");
 
 const app = express();
-
-dotenv.config({ path: "./config.env" });
 
 app.use(express.json());
 app.use(express.static("public"));
@@ -28,6 +25,6 @@ app.use(cors());
     });
     app.use(errorHandler);
   } catch (err) {
-    console.log("error: " + err);
+    console.log("error: " + err, process.env.DB_URI);
   }
 })();
